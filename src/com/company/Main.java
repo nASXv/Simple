476559b83e2566cd847041a2d6f5cbd541d6a588
nASXv <tem4ik.art@mail.ru>
2020-11-09 21:39:47 +0300

    switch to branch branch2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello, World!");
 	    System.out.println("Second commit");
+	    System.out.println("Next Commit");
+
+	    System.out.println("branch2");
     }
 }
